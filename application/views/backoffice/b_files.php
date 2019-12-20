123f6fa29156a0b562068d027c5f1c7833eeb247
software.contreras@gmail.com <software.contreras@gmail.com>
2019-12-20 17:24:19 -0500

    changes on backoffice files

--- application/views/backoffice/b_files.php
+++ application/views/backoffice/b_files.php
@@ -15,102 +15,13 @@
               <div class="element-box p-2">
                 <div class="card">
                   <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/images/presentacion.jpg';?>">
+                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/images/imagen_presentacion.jpg';?>">
                     <p class="titulo-materiais">[ES] Presentación de negocios</p> 
-                    <a href="<?php echo site_url().'static/backoffice/files/bca-capital-presentacion.pdf';?>" target="_blank" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga </a> 
+                    <a href="<?php echo site_url().'static/backoffice/files/presentacion_embajada_fk.pptx';?>" target="_blank" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga </a> 
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col-md-3">
-              <div class="element-box p-2">
-                <div class="card">
-                  <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/files/card_presentacion.jpg';?>">
-                        <p class="titulo-materiais">Tarjeta de Negocio</p> 
-                        <a href="<?php echo site_url().'static/backoffice/files/card_presentacion.jpg';?>" target="_blank" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga </a> 
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="element-box p-2">
-                <div class="card">
-                  <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/files/brand.png';?>">
-                    <p class="titulo-materiais">Marca del Negocio</p> 
-                    <a href="<?php echo site_url().'static/backoffice/files/brand.png';?>" target="_blank" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga</a>                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="element-box p-2">
-                <div class="card">
-                  <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/files/brand_black.png';?>">
-                    <p class="titulo-materiais">Marca del Negocio</p> 
-                    <a href="<?php echo site_url().'static/backoffice/files/brand_black.png';?>" target="_blank" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga</a>                    
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="element-box p-2">
-                <div class="card">
-                  <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/images/contract.png';?>">
-                        <p class="titulo-materiais">NEW CAPITAL - Contrato de Asesoramiento y Referenciación  (Inversión)</p>
-                        <a href="<?php echo site_url().'static/backoffice/files/Contrato_de_Asesoramiento_y_Referenciación_NewCapital.pdf';?>" target="_blank" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga </a> 
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="element-box p-2">
-                <div class="card">
-                  <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/images/contract.png';?>">
-                        <p class="titulo-materiais">FOREX E INVERSIONES  - Sílabus </p> 
-                        <a href="<?php echo site_url().'static/backoffice/files/silabus-forex-inversiones.pdf';?>" target="_blank" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga </a> 
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="element-box p-2">
-                <div class="card">
-                  <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/images/contract.png';?>">
-                    <p class="titulo-materiais">CONTRATO - BCA CAPITAL S.A.C. - Termino de registro - Consultor Independente</p> 
-                    <a href="javascript:void(0);" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga </a> 
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="element-box p-2">
-                <div class="card">
-                  <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/images/contract.png';?>">
-                    <p class="titulo-materiais">CONTRATO - BCA CAPITAL S.A.C. - Contrato de Distribución Independiente</p> 
-                    <a href="javascript:void(0);" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga </a> 
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="element-box p-2">
-                <div class="card">
-                  <div class="body pad-materiais" align="center"> 
-                      <img class="img-fluid img-thumbnail" src="<?php echo site_url().'static/backoffice/images/contract.png';?>">
-                    <p class="titulo-materiais">CONTRATO - BCA CAPITAL S.A.C. - Termino de registro y Plan de Compensación</p> 
-                    <a href="javascript:void(0);" class="btn btn-primary btn-sm btn round btn-simple"> Hacer Descarga </a> 
-                  </div>
-                </div>
-              </div>
-            </div>
-            
           </div>
         </div>
       </div>
