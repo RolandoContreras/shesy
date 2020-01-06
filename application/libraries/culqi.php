0276be814a1602d5c562c7e65d337a548ef131c0
software.contreras@gmail.com <software.contreras@gmail.com>
2020-01-06 14:59:44 -0500

    add libraty culqui

---
+++ application/libraries/culqi.php
@@ -0,0 +1,33 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+class Culqi {
+    public function charge($token,$price,$email,$first_name,$last_name,$address,$phone){
+        require 'Requests/library/Requests.php';
+        Requests::register_autoloader();
+        require 'culqi-php/lib/culqi.php';
+        
+        
+        $SECRET_KEY = "sk_test_RYgtwRexqbaXRpsH";
+        $culqi = new Culqi\Culqi(array('api_key' => $SECRET_KEY));
+        
+        $charge = $culqi->Charges->create(
+                array(
+                  "amount" => $price,
+                  "capture" => true,
+                  "currency_code" => "USD",
+                  "description" => "Venta de Producto FK",
+                  "email" => "$email",
+                  "installments" => 0,
+                  "antifraud_details" => array(
+                      "address" => "$address",
+                      "country_code" => "PE",
+                      "first_name" => "$first_name",
+                      "last_name" => "$last_name",
+                      "phone_number" => "$phone",
+                  ),
+                  "source_id" => "$token"
+                )
+            );
+        return $charge;
+    }
+}
+
