e89cfb5c1e87bb233fa1b811195179c235722c38
software.contreras@gmail.com <software.contreras@gmail.com>
2020-01-22 22:09:13 -0500

    changes on home

--- application/views/home.php
+++ application/views/home.php
@@ -36,9 +36,6 @@
                 <li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-22 tc-menu-item tc-menu-depth-0 tc-menu-align-left tc-menu-layout-default">
                     <a href="<?php echo site_url();?>" class="tc-menu-inner">Inicio</a>
                 </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-25 tc-menu-item tc-menu-depth-0 tc-menu-align-left tc-menu-layout-default">
-                      <a href="<?php echo site_url().'about';?>" class="tc-menu-inner">Acerca</a>
-                </li>
                 <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-48 tc-menu-item tc-menu-depth-0 tc-menu-align-left tc-menu-layout-builder">
                     <a href="<?php echo site_url().'courses';?>" class="tc-menu-inner">Cursos</a>
                   <div class='tc-megamenu-wrapper tc-megamenu-holder mega-sub-menu sub-menu'>
