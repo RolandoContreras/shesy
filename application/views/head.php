b4d7a9345ae792c86c48fccdc838486bfd1b1130
software.contreras@gmail.com <software.contreras@gmail.com>
2020-06-16 15:36:07 -0500

    cambios en head velocidad

--- application/views/head.php
+++ application/views/head.php
@@ -6,22 +6,22 @@
   <meta name="author" content="Cultura Imparable">
   <meta name="keyword" content="Cultura Imparable, Movimiento Imparable, Imparable Perú, embajadores, Imparable social, red social, mlm, mmn, culturaimprable.com, desarrollo personal, catalogo de productos, productos, plataforma de cursos online">
   <meta name="robots" content="Index, Follow">
-  <link type="text/css" media="all" href="<?php echo site_url().'static/page_front/css/autoptimize_69.css';?>" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo site_url().'static/page_front/css/autoptimize_6e.css';?>" type="text/css" media="all">
+  <link type="text/css" href="<?php echo site_url().'static/page_front/css/autoptimize_69.css';?>" rel="stylesheet" media="none" onload="if(media!='all')media='all'">
+  <link rel="stylesheet" href="<?php echo site_url().'static/page_front/css/autoptimize_6e.css';?>" type="text/css" media="none" onload="if(media!='all')media='all'">
     <!--//STAR FAVICON-->
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo site_url().'static/page_front/images/logo/favico/apple-touch-icon.png';?>">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo site_url().'static/page_front/images/logo/favico/favicon-32x32.png';?>">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo site_url().'static/page_front/images/logo/favico/favicon-16x16.png';?>">
     <link rel="manifest" href="<?php echo site_url().'static/page_front/images/logo/favico/site.webmanifest';?>">
     <!--//END FAVICON-->
-  <link rel="stylesheet" href="<?php echo site_url().'static/page_front/css/dashicons.min.css';?>" type="text/css" media="all">
-  <link rel="stylesheet" href="<?php echo site_url().'static/page_front/css/mystyle7.css';?>" type="text/css" media="all">
-  <link rel="stylesheet" id="thim-style-options-css" href="<?php echo site_url().'static/page_front/css/automatize_change1.css';?>" type="text/css" media="all">
+  <link rel="stylesheet" href="<?php echo site_url().'static/page_front/css/dashicons.min.css';?>" type="text/css" media="none" onload="if(media!='all')media='all'">
+  <link rel="stylesheet" href="<?php echo site_url().'static/page_front/css/mystyle7.css';?>" type="text/css" media="none" onload="if(media!='all')media='all'">
+  <link rel="stylesheet" id="thim-style-options-css" href="<?php echo site_url().'static/page_front/css/automatize_change1.css';?>" type="text/css" media="none" onload="if(media!='all')media='all'">
   <script src="<?php echo site_url().'static/page_front/js/jquery.js';?>"></script>
   <script>
     var lpGlobalSettings = {"localize":{"button_ok":"OK","button_cancel":"Cancel","button_yes":"Yes","button_no":"No"}};
   </script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" media="none" onload="if(media!='all')media='all'">
   <script src='<?php echo site_url().'static/page_front/js/script/login_cart.js';?>'></script>
   <script>
         var site = '<?php echo site_url();?>';
