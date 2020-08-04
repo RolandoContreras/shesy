2c10d933c03c2870e924b5837904a52cc38b9f09
software.contreras@gmail.com <software.contreras@gmail.com>
2020-08-04 18:06:58 -0500

    cambios en carrera

--- application/controllers/b_carrera.php
+++ application/controllers/b_carrera.php
@@ -1,16 +1,21 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class B_carrera extends CI_Controller {
-     function __construct() {
+
+    function __construct() {
         parent::__construct();
-        $this->load->model("customer_model","obj_customer");
-        $this->load->model("ranges_model","obj_ranges");
-        $this->load->model("unilevel_model","obj_unilevel");
-        $this->load->model("points_model","obj_points");
+        $this->load->model("customer_model", "obj_customer");
+        $this->load->model("ranges_model", "obj_ranges");
+        $this->load->model("unilevel_model", "obj_unilevel");
+        $this->load->model("points_model", "obj_points");
+        $this->load->model("commissions_model", "obj_commissions");
+        
     }
 
-    public function index()
-    {
+    public function index() {
         //GET SESION ACTUALY
         $this->get_session();
         //GET CUSTOMER_ID
@@ -19,39 +24,38 @@ class B_carrera extends CI_Controller {
         $obj_profile = $this->get_profile($customer_id);
         //GET RANGE ACTUALLY
         $params = array(
-                        "select" =>"ranges.range_id,
+            "select" => "ranges.range_id,
                                     ranges.img,
                                     ranges.name",
-                        "where" => "customer.customer_id = $customer_id and customer.status_value = 1",
-                        "join" => array('ranges, customer.range_id = ranges.range_id'),
-                        );
+            "where" => "customer.customer_id = $customer_id and customer.status_value = 1",
+            "join" => array('ranges, customer.range_id = ranges.range_id'),
+        );
         $obj_customer = $this->obj_customer->get_search_row($params);
-        
+
         //GET RANGES
         $params = array(
-                        "select" =>"ranges.range_id,
+            "select" => "ranges.range_id,
                                     ranges.name,
                                     ranges.point_grupal,
                                     ranges.img",
-                        "where" => "ranges.active = 1 and ranges.status_value = 1",
-                        "order" => "range_id ASC",
-                        );
-            $obj_range = $this->obj_ranges->search($params);
-            
-            $params = array(
-                        "select" =>"sum(point) as total_point",
-                        "where" => "customer_id = $customer_id"
-                            );
-                    $point_personal = $this->obj_points->get_search_row($params);
-                    $point = $point_personal->total_point;
-        
-        $this->tmp_backoffice->set("obj_profile",$obj_profile);
-        $this->tmp_backoffice->set("point",$point);
-        $this->tmp_backoffice->set("obj_range",$obj_range);
-        $this->tmp_backoffice->set("obj_customer",$obj_customer);
+            "where" => "ranges.active = 1 and ranges.status_value = 1",
+            "order" => "range_id ASC",
+        );
+        $obj_range = $this->obj_ranges->search($params);
+        //GET TOTAL COMMISION
+        $params = array(
+            "select" => "sum(amount) as total_comissions",
+            "where" => "customer_id = $customer_id and pago != 1");
+        //GET DATA FROM CUSTOMER
+        $obj_total_commissions = $this->obj_commissions->get_search_row($params);
+        $point = $obj_total_commissions->total_comissions;
+        $this->tmp_backoffice->set("point", $point);
+        $this->tmp_backoffice->set("obj_profile", $obj_profile);
+        $this->tmp_backoffice->set("obj_range", $obj_range);
+        $this->tmp_backoffice->set("obj_customer", $obj_customer);
         $this->tmp_backoffice->render("backoffice/b_carrera");
     }
-    
+
     public function get_profile($customer_id) {
         $params_profile = array(
             "select" => "customer.customer_id,
@@ -64,16 +68,17 @@ class B_carrera extends CI_Controller {
         //GET DATA COMMENTS
         return $obj_customer = $this->obj_customer->get_search_row($params_profile);
     }
-    
-    public function get_session(){          
-        if (isset($_SESSION['customer'])){
-            if($_SESSION['customer']['logged_customer']=="TRUE" && $_SESSION['customer']['status']=='1'){               
+
+    public function get_session() {
+        if (isset($_SESSION['customer'])) {
+            if ($_SESSION['customer']['logged_customer'] == "TRUE" && $_SESSION['customer']['status'] == '1') {
                 return true;
-            }else{
-                redirect(site_url().'home');
+            } else {
+                redirect(site_url() . 'home');
             }
-        }else{
-            redirect(site_url().'home');
+        } else {
+            redirect(site_url() . 'home');
         }
     }
+
 }
--- application/views/backoffice/b_carrera.php
+++ application/views/backoffice/b_carrera.php
@@ -1,83 +1,82 @@
 <div class="content-w">
-  <ul class="breadcrumb">
-      <li class="breadcrumb-item"> <a href="<?php echo site_url().'backoffice';?>">Tablero</a> </li>
-  </ul>
-  <div class="content-i">
-    <div class="content-box">
-      <div class="col-md-8" style="margin: 0 auto !important; margin-bottom: 20px !important;">
-        <div class="element-wrapper">
-          <h6 class="element-header">Plan Carrera<br> <small class="text-muted">Mi Avance</small> </h6>
-          <div class="element-box">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="card">
-                  <div class="body">
-                    <div class="card-value float-right text-muted tile-icon"> <i class="far fa-star" style="font-size: 60px;"></i> </div>
-                    <h3 class="mb-1"><?php echo isset($obj_customer->name) != ""?str_to_mayusculas($obj_customer->name):" Sin Rango ";?></h3>
-                    <div>Graduación Actual</div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="card">
-                  <div class="body">
-                    <div class="card-value float-right text-muted tile-icon"><i class="fas fa-award" style="font-size: 60px;"></i></div>
-                    <h3 class="mb-1"><?php echo $point;?> Puntos</h3>
-                    <div>Total de Puntos Acumulados</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-8" style="margin: 0 auto !important;">
-        <?php
-        foreach ($obj_range as $value) { ?>
-                <div id="ajaxContent">
-                      <div class="element-wrapper pb-0">
-                        <div class="element-box">
-                          <div class="row">
-                            <div class="col-md-4">
-                              <div align="center"> 
-                                  <img src='<?php echo site_url()."static/backoffice/images/rangos/$value->img";?>' width="150"> 
-                              </div>
-                            </div>
-                            <?php 
-                            $percent = ($point / $value->point_grupal)*100;
-                            $percent = format_number_miles($percent);
-                            $rest = $value->point_grupal - $point;
-                                 
-                            if($percent == 100){ ?>
-                                 <div class="col-md-8">
-                                      <h6 class="element-header" style="margin-top:15px"><?php echo str_to_mayusculas($value->name);?></h6>
-                                      <p style="color:#3E4B5B"><b>Felicidaes! Usted alcanzó esta calificación.</b> <br> Puntuación necesaria: <?php echo format_number_miles($value->point_grupal);?> Pontos de Volume <br> Premio: PIN </p>
-                                </div>    
-                            <?php }else{ ?>  
-                                  <div class="col-md-8">
-                                        <h6 class="element-header" style="margin-top:15px"><?php echo str_to_mayusculas($value->name);?></h6>
-                                      <div align="center">
-                                        <div class="form-desc">
-                                            <p class="text-primary" style="color:#3E4B5B">Faltan <?php echo format_number_miles($rest);?> Puntos de Volumen</p>
-                                        </div>
-                                      </div>
-                                      <div class="os-progress-bar warning">
-                                        <div class="bar-labels">
-                                          <div class="bar-label-left"> <?php echo $percent."%"?> </div>
-                                          <div class="bar-label-right"> <span class="info">0 /<?php echo format_number_miles($value->point_grupal);?></span> </div>
-                                        </div>
-                                        <div class="bar-level-1" style="width: 100%">
-                                          <div class="bar-level-3" style="width: <?php echo $percent."%"?>"></div>
-                                        </div>
-                                      </div>
+    <ul class="breadcrumb">
+        <li class="breadcrumb-item"> <a href="<?php echo site_url() . 'backoffice'; ?>">Tablero</a> </li>
+    </ul>
+    <div class="content-i">
+        <div class="content-box">
+            <div class="col-md-8" style="margin: 0 auto !important; margin-bottom: 20px !important;">
+                <div class="element-wrapper">
+                    <h6 class="element-header">Plan Carrera<br> <small class="text-muted">Mi Avance</small> </h6>
+                    <div class="element-box">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="card">
+                                    <div class="body">
+                                        <div class="card-value float-right text-muted tile-icon"> <i class="far fa-star" style="font-size: 60px;"></i> </div>
+                                        <h3 class="mb-1"><?php echo isset($obj_customer->name) != "" ? str_to_mayusculas($obj_customer->name) : " Sin Rango "; ?></h3>
+                                        <div>Graduación Actual</div>
                                     </div>
-                            <?php } ?>     
-                          </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card">
+                                    <div class="body">
+                                        <div class="card-value float-right text-muted tile-icon"><i class="fas fa-award" style="font-size: 60px;"></i></div>
+                                        <h3 class="mb-1"><?php echo $point; ?> Puntos</h3>
+                                        <div>Total de Puntos Acumulados</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                      </div>
                     </div>
-        <?php } ?>
-      </div>
+                </div>
+            </div>
+            <div class="col-lg-8" style="margin: 0 auto !important;">
+                <?php foreach ($obj_range as $value) { ?>
+                    <div id="ajaxContent">
+                        <div class="element-wrapper pb-0">
+                            <div class="element-box">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div align="center"> 
+                                            <img src='<?php echo site_url() . "static/backoffice/images/rangos/$value->img"; ?>' width="150"> 
+                                        </div>
+                                    </div>
+                                    <?php if ($point >= $value->point_grupal) { ?>
+                                        <div class="col-md-8">
+                                            <h6 class="element-header" style="margin-top:15px"><?php echo str_to_mayusculas($value->name); ?></h6>
+                                            <p style="color:#3E4B5B"><b>Felicidaes! Usted alcanzó esta calificación.</b> <br> Puntuación necesaria: <?php echo format_number_miles($value->point_grupal); ?> Pontos de Volume <br> Premio: PIN </p>
+                                        </div>    
+                                        <?php
+                                        } else {
+                                            $percent = ($point / $value->point_grupal) * 100;
+                                            $percent = format_number_miles($percent);
+                                            $rest = $value->point_grupal - $point;
+                                            ?>  
+                                        <div class="col-md-8">
+                                            <h6 class="element-header" style="margin-top:15px"><?php echo str_to_mayusculas($value->name); ?></h6>
+                                            <div align="center">
+                                                <div class="form-desc">
+                                                    <p class="text-primary" style="color:#3E4B5B">Faltan <?php echo format_number_miles($rest); ?> Puntos de Volumen</p>
+                                                </div>
+                                            </div>
+                                            <div class="os-progress-bar warning">
+                                                <div class="bar-labels">
+                                                    <div class="bar-label-left"> <?php echo $percent . "%" ?> </div>
+                                                    <div class="bar-label-right"> <span class="info">0 /<?php echo format_number_miles($value->point_grupal); ?></span> </div>
+                                                </div>
+                                                <div class="bar-level-1" style="width: 100%">
+                                                    <div class="bar-level-3" style="width: <?php echo $percent . "%" ?>"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php } ?>     
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
