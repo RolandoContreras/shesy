6bff2126d25f549b7056818b5c76a081623d2b40
software.contreras@gmail.com <software.contreras@gmail.com>
2020-08-30 15:00:00 -0500

    cambios en backoffice

---
+++ application/views/backoffice/head.php
@@ -0,0 +1,35 @@
+<head>
+        <title>Oficina Virtual - Cultura Imparable</title>  
+        <!--[if lt IE 11]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+        <meta name="description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos">
+        <meta name="author" content="Cultura Imparable">
+        <meta name="keyword" content="Cultura Imparable, Movimiento Imparable, Imparable Perú, embajadores, Imparable social, red social, mlm, mmn, culturaimprable.com, desarrollo personal, catalogo de productos, productos, plataforma de cursos online">
+        <meta name="author" content="Evolucion Web" />
+        <meta name="copyright" content="Evolucion Web" />
+        <link rel="stylesheet" href="<?php echo site_url().'static/backoffice/css/animate.min.css';?>">
+        <link rel="stylesheet" href="<?php echo site_url().'static/backoffice/css/style.css';?>">
+        <link rel="stylesheet" href="<?php echo site_url().'static/backoffice/css/mystyle.css';?>">
+        <link rel="stylesheet" href="<?php echo site_url().'static/backoffice/css/dropzone.css';?>" >
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <!--//STAR FAVICON-->
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo site_url() . 'static/page_front/images/logo/favico/apple-touch-icon.png'; ?>">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo site_url() . 'static/page_front/images/logo/favico/favicon-32x32.png'; ?>">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo site_url() . 'static/page_front/images/logo/favico/favicon-16x16.png'; ?>">
+        <link rel="manifest" href="<?php echo site_url() . 'static/page_front/images/logo/favico/site.webmanifest'; ?>">
+        <!--//END FAVICON-->
+        <!--//swetaler2-->
+        <link href="//cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@3/dark.css" rel="stylesheet">
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@9/dist/sweetalert2.min.js"></script>
+        <script>
+            var site = "<?php echo site_url(); ?>"
+        </script>
+        <script src="https://checkout.culqi.com/js/v3"></script>
+        <!--<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>-->
+        <script src="//code.jquery.com/jquery.min.js"></script>
+        <link href="<?php echo site_url() . "static/backoffice/dist/plugin.js" ?>" rel="stylesheet" />
+        <script src="//code.jquery.com/jquery.min.js"></script>
+        <script src="<?php echo site_url()."static/backoffice/js/circle-progress.js";?>"></script>
+    </head>
