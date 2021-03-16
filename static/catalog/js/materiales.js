6262057e930abf50fbf16b6dd94103ebe619a1e7
unknown <software.contreras@gmail.com>
2021-03-16 11:43:03 -0500

    cambiar 25 por 5 nivel, error en texto. Materiales JS

--- static/catalog/js/materiales.js
+++ static/catalog/js/materiales.js
@@ -58,7 +58,7 @@ function show_information() {
                 
                 texto = texto+'<div class="col-sm-4">';
                 texto = texto+'<div class="form-group"> ';
-                texto = texto+'<label class="control-label">Puntos 25°</label> ';
+                texto = texto+'<label class="control-label">Puntos 5°</label> ';
                 texto = texto+'<input type="text" class="form-control" value="'+ data.obj_catalog.bono_n5 +'" disabled="">';
                 texto = texto+'</div>';   
                 
