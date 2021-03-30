e0cf0c36a62187460ed72b01b4f1da7d987b3d0a
unknown <software.contreras@gmail.com>
2021-03-30 16:51:17 -0500

    cambios en registro

--- application/views/register.php
+++ application/views/register.php
@@ -9,47 +9,47 @@
         <meta name="author" content="Cultura Imparable">
         <meta name="keyword" content="Cultura Imparable, Movimiento Imparable, Imparable Perú, embajadores, Imparable social, Imparable corporación, mlm, mmn, culturaimprable.com">
         <meta name="robots" content="Index, Follow">
-        <meta content="width=device-width, initial-scale=1" name="viewport">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="author" content="Evolution Web">
         <meta name="googlebot" content="index, follow" />
-    <meta name="author" content="Evolution Web">
-    <meta name="distribution" content="Global">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="<?php echo site_url();?>">
-    <meta property="og:title" content="Cultura Emprendedora | Organización Neuronal de emprendedores">
-    <meta property="og:url" content="<?php echo site_url();?>">
-    <meta property="og:description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos">
-    <meta property="og:image" content="<?php echo site_url().'static/page_front/images/logo/logo-fuego.png';?>">
-    <meta property="og:type" content="video_lecture">
-    <meta property="og:site_name" content="Cultura Emprendedora">
-    <meta property="og:locale" content="es_PE">
-    <meta itemprop="name" content="Cultura Emprendedora | Organización Neuronal de emprendedores">
-    <meta itemprop="url" content="<?php echo site_url();?>">
-    <meta itemprop="description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos">
-    <meta itemprop="image" content="<?php echo site_url().'static/page_front/images/logo/logo-fuego.png';?>">
-    <meta name="description" content="Cultura Emprendedora - Organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica"> 
-    <meta name="keywords" content="Cultura Emprendedora, Movimiento Emprendedor, Cultura Emprendedora Perú, Embajadores, Emprendedor Social, red social, mlm, culturaemprendedora.online, desarrollo personal, catalogo de productos, productos, plataforma de cursos online"> 
-    <meta name="author" content="Evolucion Web"> 
-    <meta name="robots" content="follow, index"/> 
-    <meta name="googlebot" content="index, follow" /> 
-    <meta property="og:title" content="Cultura Emprendedora | Organización Neuronal de emprendedores"> 
-    <meta property="og:url" content="<?php echo site_url(); ?>"> 
-    <meta property="og:description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos"> 
-    <meta property="og:image" content="<?php echo site_url().'static/page_front/images/logo/logo-fuego.png';?>"> 
-    <meta property="og:type" content="website"> 
-    <meta property="og:site_name" content="Cultura Emprendedora"> 
-    <meta property="og:locale" content="es"> 
-    <meta itemprop="name" content="Cultura Emprendedora | Organización Neuronal de emprendedores"> 
-    <meta itemprop="url" content="<?php echo site_url();?>"> 
-    <meta itemprop="description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos"> 
-    <meta itemprop="image" content="<?php echo site_url().'static/page_front/images/logo/logo-fuego.png';?>"> 
-    <meta name="twitter:card" content="summary_large_image"> 
-    <meta name="twitter:title" content="Cultura Emprendedora | Organización Neuronal de emprendedores"> 
-    <link rel="canonical" href="<?php echo site_url(); ?>" /> 
+        <meta name="author" content="Evolution Web">
+        <meta name="distribution" content="Global">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="<?php echo site_url();?>">
+        <meta property="og:title" content="Cultura Emprendedora | Organización Neuronal de emprendedores">
+        <meta property="og:url" content="<?php echo site_url();?>">
+        <meta property="og:description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos">
+        <meta property="og:image" content="<?php echo site_url().'static/page_front/images/logo/logo-fuego.png';?>">
+        <meta property="og:type" content="video_lecture">
+        <meta property="og:site_name" content="Cultura Emprendedora">
+        <meta property="og:locale" content="es_PE">
+        <meta itemprop="name" content="Cultura Emprendedora | Organización Neuronal de emprendedores">
+        <meta itemprop="url" content="<?php echo site_url();?>">
+        <meta itemprop="description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos">
+        <meta itemprop="image" content="<?php echo site_url().'static/page_front/images/logo/logo-fuego.png';?>">
+        <meta name="description" content="Cultura Emprendedora - Organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica"> 
+        <meta name="keywords" content="Cultura Emprendedora, Movimiento Emprendedor, Cultura Emprendedora Perú, Embajadores, Emprendedor Social, red social, mlm, culturaemprendedora.online, desarrollo personal, catalogo de productos, productos, plataforma de cursos online"> 
+        <meta name="author" content="Evolucion Web"> 
+        <meta name="robots" content="follow, index"/> 
+        <meta name="googlebot" content="index, follow" /> 
+        <meta property="og:title" content="Cultura Emprendedora | Organización Neuronal de emprendedores"> 
+        <meta property="og:url" content="<?php echo site_url(); ?>"> 
+        <meta property="og:description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos"> 
+        <meta property="og:image" content="<?php echo site_url().'static/page_front/images/logo/logo-fuego.png';?>"> 
+        <meta property="og:type" content="website"> 
+        <meta property="og:site_name" content="Cultura Emprendedora"> 
+        <meta property="og:locale" content="es"> 
+        <meta itemprop="name" content="Cultura Emprendedora | Organización Neuronal de emprendedores"> 
+        <meta itemprop="url" content="<?php echo site_url();?>"> 
+        <meta itemprop="description" content="Somos la primera organización Neuronal de EMPRENDEDORES con PROPÓSITO en Latinoamérica, compartimos una cultura donde se crean en los sueños y se compartan herramientas para el logro de estos mismos"> 
+        <meta itemprop="image" content="<?php echo site_url().'static/page_front/images/logo/logo-fuego.png';?>"> 
+        <meta name="twitter:card" content="summary_large_image"> 
+        <meta name="twitter:title" content="Cultura Emprendedora | Organización Neuronal de emprendedores"> 
+        <link rel="canonical" href="<?php echo site_url(); ?>" /> 
         <!--//STAR FAVICON-->
         <link rel="apple-touch-icon" sizes="180x180" href="<?php echo site_url() . 'static/page_front/images/logo/favico/apple-touch-icon.png'; ?>">
         <link rel="icon" type="image/png" sizes="32x32" href="<?php echo site_url() . 'static/page_front/images/logo/favico/favicon-32x32.png'; ?>">
         <link rel="icon" type="image/png" sizes="16x16" href="<?php echo site_url() . 'static/page_front/images/logo/favico/favicon-16x16.png'; ?>">
-        <link rel="manifest" href="<?php echo site_url() . 'static/page_front/images/logo/favico/site.webmanifest'; ?>">
         <!--//END FAVICON-->
         <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet" type="text/css">
         <link href="<?php echo site_url() . 'static/page_front/css/login/main.css?version=4.4.0'; ?>" rel="stylesheet">
